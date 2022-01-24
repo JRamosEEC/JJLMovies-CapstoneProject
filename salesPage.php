a9041647c17afdd6927af94d6ee269334bd030aa
Ramos <jdramos1@email.neit.edu>
2022-01-24 12:16:59 -0500

    reformat

--- salesPage.php
+++ salesPage.php
@@ -1,37 +1,5 @@
 <?php
     require (__DIR__ . "/dbQuery.php");
-
-    $inventoryData = getInventory();
-    $invCategories = getCategories();
-
-    $action = $_GET["action"] ?? '';
-    $id = $_GET["id"] ?? '';
-
-    if($action == 'bought')
-    {
-        foreach($inventoryData as $key => $value)
-        {
-            if($value['productId'] == $id)
-            {
-                $price  = $value['price'];
-                $tax = $value['price']*0.0625;
-
-                $qty = $value['quantity'];
-            }
-        }
-
-        if($qty >0)
-        {
-            addSales($id, '3', '0', $tax, $price);
-            header("Location: salesPage.php?id=None");
-            exit();
-        }
-        else
-        {
-            header("Location: salesPage.php?id=None");
-            exit();
-        }
-    }
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ramos Inventory Management</title>
+    <title></title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -79,7 +47,7 @@
                     <a href="inventoryPage.php" class="row no-marginR"><img id='navIcon' class="text-center no-pad col" src="images/inventory-icon.png"><p class="text-center no-margin col">Inventory</p><img id='navArrow' class="text-center col" src="images/right-arrow.png"></a>
                 </li>
                 <li id='copyrights'>
-                    © Justin Ramos
+                    
                 </li>
             </ul>
         </nav>
@@ -94,89 +62,10 @@
                 </nav>
 
                 <div class="header center col-4">
-                    Sales
+                    *Title*
                 </div>
 
                 <div id="spacer" class="col-4"></div>
-            </div>
-
-            <div class="row no-marginL">
-                <div id="salesContainerL" class="row col-6 no-marginL">
-                    <div class="row col-6">
-                        <div id="fieldDesc" class="col-6">Category: </div>
-                        <div id="fieldInvCat">
-                            <select>
-                                <option value='id'>ID</option>
-                                <option value='qty'>Quantity</option>
-                                <option value='name'>Name</option>
-                                <option value='category'>Category</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="row col-6 no-margin no-pad">
-                        <div id="fieldDesc" class="col-6">Search: </div>
-                        <textarea id="invSearch" rows="1" cols="17" wrap="off" placeholder="Input Search"></textarea>
-                    </div>
-                </div>
-
-                <div id="salesContainerIcon" class="row col-6 no-marginL align-items-end bottomLine">
-                    <img id='salesIcon' class="text-center no-pad col" src="images/cart-icon.png">
-                </div>
-            </div>
-
-            <div class="row no-marginL">
-                <div id="invContainerSales" class="row col-6 no-marginL">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Quantity</th>
-                                <th>Name</th>
-                                <th>Category</th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <?PHP foreach($inventoryData as $key => $value): ?>
-                                <?php echo '<tr><td>' . $value['productId'] . '</td><td>' . $value['quantity'] . '</td><td>' . $value['name'] . '</td><td>' . $value['category'] . '</td><td class="centerIcon"><a href="salesPage.php?id=' . $value['productId'] . '" style="text-decoration: none;"><i class="fas fa-shopping-basket fa-w-10 fa-2x"></i></a></td></tr>'; ?>
-                            <?PHP endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-
-                <div id="salesContainerR" class="row col-6 no-marginL">
-                    <div id="salesHeader" class="row no-marginL">
-                        <div class="col-6 text-center">Product ID : <?php foreach($inventoryData as $key => $value){if($value['productId'] == $_GET['id']){echo $value['productId'];}}?></div>
-                        <div class="col-6 text-center">Name : <?php foreach($inventoryData as $key => $value){if($value['productId'] == $_GET['id']){echo $value['name'];}}?></div>
-                    </div>
-
-                    <div id="salesPrice" class="row no-marginL">
-                        <div class="col-12 text-center">Price: $<?php foreach($inventoryData as $key => $value){if($value['productId'] == $_GET['id']){echo $value['price'];}}?></div>
-                    </div>
-
-                    <div id="salesDiscount" class="row no-marginL">
-                        <div id="salesDiscountName" class="col-5">Discount : </div>
-                        <div id="salesDiscountField" class="col-7">
-                            <select>
-                                <option value="" disabled selected hidden>Select Discount</option>
-                                <option value="0">None</option>
-                                <option value="1">None</option>
-                                <option value="2">None</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div id="salesTotals" class="row no-marginL">
-                        <div class="col-6 text-center">Tax : $<?php foreach($inventoryData as $key => $value){if($value['productId'] == $_GET['id']){echo $value['price']*0.0625;}}?></div>
-                        <div class="col-6 text-center">Total : $<?php foreach($inventoryData as $key => $value){if($value['productId'] == $_GET['id']){echo $value['price']+$value['price']*0.0625;}}?></div>
-                    </div>
-
-                    <div id="salesBuyButtonContainer" class="row no-marginL">
-                        <div id="buyButton" class="col-12"><a href=<?php if ($_GET["id"] != 'None'){echo '"salesPage.php?action=bought&amp;id=' . $_GET["id"] . '"';} ?>><img id='buyButtonImg' class="text-center" src="images/buy-button.png"></a></div>
-                    </div>
-                </div>
             </div>
         </div>
 
