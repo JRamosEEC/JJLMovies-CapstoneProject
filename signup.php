56ebd9f7150d62654b953f79da032817abcc3f6a
Ramos <jdramos1@email.neit.edu>
2022-02-01 13:22:23 -0500

    signup page nav

--- signup.php
+++ signup.php
@@ -1,37 +1,128 @@
+<?php
+    require (__DIR__ . "/dbQuery.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>SIGN UP</h1>
-    <div>
-        <input type="text" placeholder="Create Username">
-    </div>
-    <div>
-        <input type="text" placeholder="Create Password">
-    </div>
+    <title>Ramos Inventory Management</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style.css">
     
-    <div>
-        <input type="text" placeholder="Email">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <?php include(__DIR__ . "/nav.php")?>
+
+        <!-- Page Content -->
+        <div id="content">
+            <?php include(__DIR__ . "/header.php")?>
+
+            <h1>SIGN UP</h1>
+
+            <div>
+                <input type="text" placeholder="Create Username">
+            </div>
+            <div>
+                <input type="text" placeholder="Create Password">
+            </div>
+            
+            <div>
+                <input type="text" placeholder="Email">
+            </div>
+
+            <div>
+                <input type="text" placeholder="Birthday">
+            </div>
+
+            <div>
+                <input type="text" placeholder="First Name">
+            </div>
+
+            <div>
+                <input type="text" placeholder="Last Name">
+            </div>
+            <div>
+                <input type="button" value="Create Account">
+            </div>
+        </div>
     </div>
 
-    <div>
-        <input type="text" placeholder="Birthday">
-    </div>
+    <script>
+        $(document).ready(function () {
 
-    <div>
-        <input type="text" placeholder="First Name">
-    </div>
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
 
-    <div>
-        <input type="text" placeholder="Last Name">
-    </div>
-    <div>
-        <input type="button" value="Create Account">
-    </div>
+            var element = document.getElementById('sidebarCollapse');
+            var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+            if (width > 768){
+                if($('#sidebar').attr("class")){
+                    element.innerHTML = '&#8250;';
+                }
+                else{
+                    element.innerHTML = '&#8249;';
+                }
+            }
+            else{
+                if($('#sidebar').attr("class")){
+                    element.innerHTML = '&#8249;';
+                }
+                else{
+                    element.innerHTML = '&#8250;';
+                }
+            }
+        });
+
+        var width = $(window).width();
+        $(window).on('resize', function() {
+            if ($(this).width() !== width) {
+                width = $(this).width();
+
+                var element = document.getElementById('sidebarCollapse');
+
+                if (width < 768){
+                    if($('#sidebar').attr("class")){
+                        element.innerHTML = '&#8249;';
+                    }
+                    else{
+                        element.innerHTML = '&#8250;';
+                    }
+                }
+                else{
+                    if($('#sidebar').attr("class")){
+                        element.innerHTML = '&#8250;';
+                    }
+                    else{
+                        element.innerHTML = '&#8249;';
+                    }
+                }
+        }
+        });
+
+        });
+    </script>
 </body>
 </html>
