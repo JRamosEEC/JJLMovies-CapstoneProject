36a3a64e1d1d2e941ae4a062aad0fa93a7708b81
Ramos <jdramos1@email.neit.edu>
2022-02-01 13:46:16 -0500

    style.css and signup.php

--- signup.php
+++ signup.php
@@ -41,7 +41,7 @@
 
             <div id="spacer" class="col-4"></div>
 
-            <div id="signupContainer" class="col-4 center">
+            <div id="signupContainer" class="col-4 ">
                 <h1>SIGN UP</h1>
 
                 <div>
