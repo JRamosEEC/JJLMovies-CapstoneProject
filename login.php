72398dbc66f66ef6ac92870c7575141038e67adf
Ramos <jdramos1@email.neit.edu>
2022-02-01 13:55:02 -0500

    style.css and signup.php

--- login.php
+++ login.php
@@ -18,25 +18,16 @@
     </style>
 </head>
 
-
-   
-
-
-
-
-</head>
 <body> 
-<!-- jQuery CDN - Slim version (=without AJAX) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 
-
-
-<div class="wrapper">
+    <div class="wrapper">
         <!-- Sidebar -->
         <?php include(__DIR__ . "/nav.php")?>
 
@@ -45,32 +36,24 @@
             <?php include(__DIR__ . "/header.php")?>
 
             <div id="Logs">
-        <h1 style="color:white">LOG IN  </h1>
+                <h1 style="color:white">LOG IN</h1>
 
-        <div id="username">
-            <input type="text" name="Username" placeholder="Username">
-        </div>
+                <div id="username">
+                    <input type="text" name="Username" placeholder="Username">
+                </div>
 
-        <div id="pwrd">
-            <input type="password" name="Password" placeholder="Password">
-        </div>
-    <br>
-        <div id="btnLogs">
-            <input type="button" name="loginbtn" value="Login">
-            <input type="button" name="signupbtn" value="Create Account">
-        </div>
+                <div id="pwrd">
+                    <input type="password" name="Password" placeholder="Password">
+                </div>
 
-
-        </div>
-
-    </div>   
-    
+                <br>
+                
+                <div id="btnLogs">
+                    <input type="button" name="loginbtn" value="Login">
+                    <input type="button" name="signupbtn" value="Create Account">
+                </div>
+            </div>
+        </div>   
     </div>
-
-   
-
-
-
-
 </body>
 </html>
