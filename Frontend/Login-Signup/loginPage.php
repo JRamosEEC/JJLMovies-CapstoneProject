2316d7499e4b67741f51a330a57b880a72177b7f
Jacob Botelho <jabotelho@email.neit.edu>
2022-02-11 12:55:26 -0500

    LOGIN CHANGES (SAME AS SIGNUP)

--- Frontend/Login-Signup/loginPage.php
+++ Frontend/Login-Signup/loginPage.php
@@ -1,63 +1,108 @@
 <?php
-    require (__DIR__ . "/../../Backend/dbQuery.php");
+require (__DIR__ . "/../../Backend/dbQuery.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LogIn</title> 
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="../CSS/style.css">
-    
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-    <style>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title> 
+<!-- Bootstrap CSS CDN -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<!-- Our Custom CSS -->
+<link rel="stylesheet" href="../CSS/style.css">
 
-    </style>
+<!-- Font Awesome JS -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+<style>
+
+</style>
 </head>
 
 <body> 
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <!-- Popper.JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 
-    <div class="wrapper">
-        <!-- Sidebar -->
-        <?php include(__DIR__ . "/../Blueprints/navBlueprint.php")?>
+<div class="wrapper">
+    <!-- Sidebar -->
+    <?php include(__DIR__ . "/../Blueprints/navBlueprint.php")?>
 
-        <!-- Page Content -->
-        <div id="content">
-            <?php include(__DIR__ . "/../Blueprints/headerBlueprint.php")?>
+    <!-- Page Content -->
+    <div id="content">
+        <?php include(__DIR__ . "/../Blueprints/headerBlueprint.php")?>
 
-            <div id="Logs">
-                <h1 style="color:white">LOG IN</h1>
+        <div id="Logs">
+            <h1 style="color:white">LOGIN</h1>
 
-                <div id="username">
-                    <input type="text" name="Username" placeholder="Username">
-                </div>
-
-                <div id="pwrd">
-                    <input type="password" name="Password" placeholder="Password">
-                </div>
-
-                <br>
-                
-                <div id="btnLogs">
-                    <a id="BtnLogin" href="" class="btn btn-primary">Login</a>
-                    <a id="BtnCreateAccount" href="signupPage.php" class="btn btn-primary">Create Account</a>
-                </div>
+            <div id="username">
+                <input type="text" name="Username" placeholder="Username">
             </div>
-        </div>   
-    </div>
+
+            <div id="pwrd">
+                <input type="password" name="Password" placeholder="Password">
+            </div>
+
+            <br>
+            
+            <div id="btnLogs">
+                <a id="BtnLogin" href="" class="btn btn-primary">Login</a>
+                <a id="BtnCreateAccount" href="signupPage.php" class="btn btn-primary">Create Account</a>
+
+
+
+            </div>
+
+
+        </div>
+
+        <div id="formContainer" class="row center no-margin no-padL">
+            <h1 class="col-xl-6 center">Login</h1>
+
+            <div id="spacer" class="col-3"></div>
+            
+            <form action="" method="post" class="col-6">
+                <div class="row">
+       
+                    <div id="formContainer" class="col-xl-6">
+                        <div class="row">
+                            <div class="col-12 center">Username</div>
+
+                            <div class="col-12 center">
+                                <input type="text" name="username">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="formContainer" class="col-xl-6">
+                        <div class="row">
+                            <div class="col-12 center">Password</div>
+
+                            <div class="col-12 center">
+                                <input type="text" name="password">
+                            </div>
+                        </div>
+                    </div>  
+
+                    
+                    <div id="formContainer" class="col-xl-6 center">
+                        <input type="submit" class="largeBtn" value="Create Account">
+                    </div>
+                </div>
+            </form>
+
+            <div id="spacer" class="col-3"></div>
+        </div>
+
+
+    </div>   
+</div>
 </body>
 </html>
