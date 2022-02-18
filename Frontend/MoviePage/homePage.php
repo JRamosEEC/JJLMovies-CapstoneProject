6e5eacef5929883c2ba8db7b72b0157b2e3b304e
Ramos <jdramos1@email.neit.edu>
2022-02-18 12:20:47 -0500

    Fixed page titles -Justin

--- Frontend/MoviePage/homePage.php
+++ Frontend/MoviePage/homePage.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JJl Movie Reviews</title>
+    <title>JJL Movie Reviews</title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
