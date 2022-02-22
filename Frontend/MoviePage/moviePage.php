22d765dede2d5ac94e2125b495a3c9beec1b8194
Kibet <ekkibet@email.neit.edu>
2022-02-22 14:08:50 -0500

    Week 7 Elvis

--- Frontend/MoviePage/moviePage.php
+++ Frontend/MoviePage/moviePage.php
@@ -39,25 +39,40 @@
         <div id="content">
             <?php include(__DIR__ . "/../Blueprints/headerBlueprint.php")?>
             <?php foreach($details as $row) :?>
-                <div class="row" id="itemContainer">
-                    <img src=<?php echo $row['CoverIMG']; echo $row['MovieDescription'];?> id="trendImg" width=275px height="390px"; >
-                    
-                    <div class="col">  
-                            Title: <?php echo $row['MovieTitle'];?>
+                <div class="row">
+                    <div class="col-xl-4">
+                        <img src=<?php echo $row['CoverIMG'];?> id="trendImg" width=275px height="390px"; >
                     </div>
+                    
+                    <div class="col-xl-8">  
+                        <div class="row" id="itemContainer2">  
+                            <div class="col">  
+                                    Title: <?php echo $row['MovieTitle'];?>
+                            </div>
 
-                    <div class="col">  
-                            Creator: <?php echo $row['CreatorName'];?>
-                    </div>
+                            <div class="col">  
+                                    Creator: <?php echo $row['CreatorName'];?>
+                            </div>
 
-                    <div class="col">  
-                            Rating: <?php echo $row['LikeCount'];?>
-                    </div>
-                    
-                    
-                            
-                    
+                            <div class="col">  
+                                    Rating: <?php echo $row['LikeCount'];?>
+                            </div>
+                        </div>
+
+                        <div class="row" id="itemContainer2">
+                                <h2 style="width:100%">Description</h2>
+
+                                <p style="width:100%"><?php echo $row['MovieDescription']; ?></p>
+                        </div>
+                    </div>               
                          <!--- it's be width x height in html not length but for now to avoid stretching images let them size themselvs --->
+                </div>
+                <div id="itemContainer2">
+                    <h2>Ratings<h2>
+                        <div class="row">
+                            <input class="btn btn-primary" type="button" value="Write A Review">
+                        </div>
+                    
                 </div>
 
                 <?php endforeach ?>
