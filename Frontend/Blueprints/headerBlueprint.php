9022394147a68f05f2e95a04efa03dba5ac2a4f0
JRamosEEC <jdramso1@email.neit.edu>
2022-02-24 02:16:11 -0500

    Reworked nav bar -Justin

--- Frontend/Blueprints/headerBlueprint.php
+++ Frontend/Blueprints/headerBlueprint.php
@@ -1,9 +1,12 @@
-<div id="title" class="row header centerV no-marginL">
-    <nav class="transparent centerV no-padL navbar-expand-lg navbar-light bg-light col-4">
-        <a class="btn btn-primary" id="sidebarCollapse">&#8249;</a>
+<script src="https://kit.fontawesome.com/18ddcc2bb6.js" crossorigin="anonymous"></script>
+
+<div id="PageHeader" class="row header centerV no-marginL">
+    <nav class="col transparent centerV no-padL navbar-expand-lg navbar-light bg-light col-4">
+        <a class="btn btn-primary" id="sidebarCollapseHead"><i class="fa-solid fa-bars"></i></a>
     </nav>
 
     <div id="head1" class="header center col-4">
+
     <?php $header="";
     echo $header?>
 
@@ -17,4 +20,13 @@
         <a href="/Frontend/Login-Signup/loginPage.php" class="btn btn-primary">Login</a>
     </div>
 </div>
-            
+
+<script>
+    $(document).ready(function () {
+
+    $('#sidebarCollapseHead').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#fadeLayer').toggleClass('active');
+    });
+    });
+</script>
--- Frontend/Blueprints/navBlueprint.php
+++ Frontend/Blueprints/navBlueprint.php
@@ -1,84 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JJL Movie Reviews</title>
-</head>
-<body>
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <a href="/Frontend/MoviePage/homePage.php" class="row no-marginR"><img id='navIcon' class="text-center no-pad col" src="/images/logo-icon.png"></a>
-            <h3 class="text-center">Navigation</h3>
-            
+<div id="fadeLayer"></div>
+
+<nav id="sidebar">
+    <div id="sidebarHeader" class="row no-margin">
+        <div class="col no-margin no-pad">
+            <a id="sidebarCollapseNav" class="btn btn-primary"><i class="fa-solid fa-bars"></i></a>
         </div>
 
-        <ul class="list-unstyled components">
-            <li>
-                <a href="/Frontend/MoviePage/homePage.php" class="row current-page no-marginR"><img id='navIcon' class="text-center no-pad col" src="/images/home-icon.png"><img id='navArrow' class="text-center col" src="/images/right-arrow.png"></a>
-            </li>
-            <li>
-                <a href="/Frontend/MoviePage/highlightsPage.php" class="row no-marginR"><img id='navIcon' class="text-center no-pad col" src="/images/highlights-icon.png"><img id='navArrow' class="text-center col" src="/images/right-arrow.png"></a>
-            </li>
-        </ul>
-    </nav>
+        <a href="/Frontend/MoviePage/homePage.php" class="col no-margin"><img id='navIcon' class="text-center no-pad col" src="/images/logo-icon.png"></a>
+    </div>
 
-    <script>
-        $(document).ready(function () {
+    <ul class="list-unstyled components">
+        <li>
+            <a href="/Frontend/MoviePage/homePage.php" class="row current-page no-marginR"><img id='navIcon' class="text-center no-pad col" src="/images/home-icon.png"><img id='navArrow' class="text-center col" src="/images/right-arrow.png"></a>
+        </li>
+        <li>
+            <a href="/Frontend/MoviePage/highlightsPage.php" class="row no-marginR"><img id='navIcon' class="text-center no-pad col" src="/images/highlights-icon.png"><img id='navArrow' class="text-center col" src="/images/right-arrow.png"></a>
+        </li>
+    </ul>
+</nav>
 
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
+<script>
+    $(document).ready(function () {
 
-            var element = document.getElementById('sidebarCollapse');
-            var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-            if (width > 768){
-                if($('#sidebar').attr("class")){
-                    element.innerHTML = '&#8250;';
-                }
-                else{
-                    element.innerHTML = '&#8249;';
-                }
-            }
-            else{
-                if($('#sidebar').attr("class")){
-                    element.innerHTML = '&#8249;';
-                }
-                else{
-                    element.innerHTML = '&#8250;';
-                }
-            }
-        });
-
-        var width = $(window).width();
-        $(window).on('resize', function() {
-            if ($(this).width() !== width) {
-                width = $(this).width();
-
-                var element = document.getElementById('sidebarCollapse');
-
-                if (width < 768){
-                    if($('#sidebar').attr("class")){
-                        element.innerHTML = '&#8249;';
-                    }
-                    else{
-                        element.innerHTML = '&#8250;';
-                    }
-                }
-                else{
-                    if($('#sidebar').attr("class")){
-                        element.innerHTML = '&#8250;';
-                    }
-                    else{
-                        element.innerHTML = '&#8249;';
-                    }
-                }
-        }
-        });
-
-        });
-    </script>
-</body>
-</html>
-
+    $('#sidebarCollapseNav').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#fadeLayer').toggleClass('active');
+    });
+    });
+</script>
