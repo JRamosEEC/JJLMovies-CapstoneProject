9d919501fcd154368b400bc41a31e7d5704c3651
Ramos <jdramos1@email.neit.edu>
2022-03-08 14:44:31 -0500

    Header search box fix -Justin

--- Frontend/Blueprints/searchResults.php
+++ Frontend/Blueprints/searchResults.php
@@ -7,9 +7,36 @@
 
     $resultHtmlStr = "";
 
-    foreach($searchResults as $row){
-        $resultHtmlStr=$row['UserAccountID'];//getting the userAccount id from the accounts table
+    foreach ($searchResults as $row):
+    {
+        $resultHtmlStr += '
+        <div id="searchItem" class="row no-margin no-pad">
+            <div class="col-sm">
+                <div class="row">
+                    <div id="searchComponentMovieImg" class="col-auto no-pad">
+                        <img src="' . $row["CoverImg"] . '" width=75px;>
+                    </div>
+
+                    <div id="searchComponentMovieDetailsContainer" class="col-auto ml-4">
+                        <div id="searchComponentDetails" class="row">
+                            <div style="font-size: 20px;">"' . $row["MovieTitle"] . '"</div>
+                        </div>
+                    
+                        <div id="searchComponentDetails" class="row">
+                            <strong>Creator :</strong><div> &nbsp;"' . $row["CreatorName"] . '"</div>
+                        </div>
+
+                        <div id="searchComponentDetails" class="row">
+                            <strong>Rating :</strong><div> &nbsp;</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="spacerLine" class="row no-margin" style="margin-top: 10px !important; margin-bottom: 5px !important;"></div>
+        ';
     }
 
-    echo $searchTxt;
+    echo $resultHtmlStr;
 ?>
