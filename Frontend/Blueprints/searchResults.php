bf9c3b3e23efeda7f00b94156c6b3bbf88d427a7
Kibet <ekkibet@email.neit.edu>
2022-03-11 14:00:32 -0500

    Elvis week 9

--- Frontend/Blueprints/searchResults.php
+++ Frontend/Blueprints/searchResults.php
@@ -7,16 +7,18 @@
 
     $resultHtmlStr = "";
 ?>
+
 <?php
     foreach ($searchResults as $row)
     {
+        $resultHtmlStr .= '<a href="/Frontend/MoviePage/moviePage.php?id=' . $row["MovieID"] . '">';
         $resultHtmlStr .= '<div id="searchItem" class="row no-margin no-pad">';
         $resultHtmlStr .= '<div class="col-sm">';
         $resultHtmlStr .= '<div class="row">';
         $resultHtmlStr .= '<div id="searchComponentMovieImg" class="col-auto no-pad">';
         $resultHtmlStr .= '<img src="' . $row["CoverIMG"] . '" width=75px;>';
         $resultHtmlStr .= '</div>';
-        
+
         $resultHtmlStr .= '<div id="searchComponentMovieDetailsContainer" class="col-auto ml-4">';
         $resultHtmlStr .= '<div id="searchComponentDetails" class="row">';
         $resultHtmlStr .= '<div style="font-size: 20px;">' . $row["MovieTitle"] . '</div>';
@@ -35,6 +37,7 @@
         $resultHtmlStr .= '</div>';
 
         $resultHtmlStr .= '<div id="spacerLine" class="row no-margin" style="margin-top: 10px !important; margin-bottom: 5px !important;"></div>';
+        $resultHtmlStr .= '</a>';
     }
 
     echo $resultHtmlStr;
