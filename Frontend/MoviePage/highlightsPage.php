9d2c364236c769266a631d4134b25543195fd4f7
Kibet <ekkibet@email.neit.edu>
2022-03-15 11:49:22 -0400

    elvis week 0

--- Frontend/MoviePage/highlightsPage.php
+++ Frontend/MoviePage/highlightsPage.php
@@ -46,7 +46,7 @@
                 
                     <?php foreach($trend as $row) :?>
                         <div class="col-sm-3" id="trends">
-                            <a href="moviePage.php?id=<?php echo $row['MovieID'];?>"><img src='../../uploads/<?php echo $row['CoverIMG']; ?>' id="trendImg" width="265px" height="385px"; ></a>
+                            <a href="moviePage.php?id=<?php echo $row['MovieID'];?>"><img src='../../uploads/<?php echo $row['CoverIMG']; ?>' id="trendImg" width="240px" height="360px"; ></a>
 
                             <div id="trendInfo" class="row">
 
